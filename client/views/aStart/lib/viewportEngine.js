eb58ab6aba6fe78b9811a69c6983bc2bd4452c23
stefanosalvucci <stefano.salvucci@gmail.com>
2015-11-17 12:11:57 +0100

    added bush constraints, fix padding

--- client/views/aStart/lib/viewportEngine.js
+++ client/views/aStart/lib/viewportEngine.js
@@ -5,21 +5,33 @@ var initViewportEngine = function(){
   gameFunctions.moveSpriteRight = function(){
     gameFunctions.flip(1);
     $('#sprite-image').attr("src", "images/mario_move.gif");
-    move('#sprite')
-      .add('margin-left', 30)
-      .end(function(){
+    if (parseInt($('#sprite').css("margin-left")) < 600) {
+      move('#sprite')
+        .add('margin-left', 30)
+        .end(function(){
+          $('#sprite-image').attr("src", "images/mario_stop.png");
+        });
+    } else {
+      setTimeout(function(){
         $('#sprite-image').attr("src", "images/mario_stop.png");
-      });
+      }, 800);
+    };
   };
 
   gameFunctions.moveSpriteLeft = function(){
     gameFunctions.flip(-1);
     $('#sprite-image').attr("src", "images/mario_move.gif");
-    move('#sprite')
-      .sub('margin-left', 30)
-      .end(function(){
+    if (parseInt($('#sprite').css("margin-left")) > 70) {
+      move('#sprite')
+        .sub('margin-left', 30)
+        .end(function(){
+          $('#sprite-image').attr("src", "images/mario_stop.png");
+        });
+    } else {
+      setTimeout(function(){
         $('#sprite-image').attr("src", "images/mario_stop.png");
-      });
+      }, 800);
+    }
   }
 
   gameFunctions.flip = function(direction){

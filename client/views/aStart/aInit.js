08d1f4af9086e70b233804adbb04eb8d656ec566
stefanosalvucci <stefano.salvucci@gmail.com>
2015-11-17 20:33:35 +0100

    fix move.js init

--- client/views/aStart/aInit.js
+++ client/views/aStart/aInit.js
@@ -1,9 +1,6 @@
 Meteor.startup(function() { // wait for app's environment to be loaded
 
 	window.initClientA = function () {
-		// load move.js (deferred <-- public)
-		$('body').append('<script type="text/javascript" src="lib/move.min.js">');
-
 		// init script engine
 		initScriptEngine();
 
